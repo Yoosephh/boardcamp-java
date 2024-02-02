c29a147f2a8f1dc69aaf36c41bb5ebc68ad27292
José Eduardo <105430849+Yoosephh@users.noreply.github.com>
2024-02-01 22:07:44 -0300

    feat: models

---
+++ src/main/java/com/boardcamp/api/models/RentalsModel.java
@@ -0,0 +1,45 @@
+package com.boardcamp.api.models;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "rentals")
+public class RentalsModel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "customerId")
+  private CustomerModel customerId;
+
+  @ManyToOne
+  @JoinColumn(name = "gameId")
+  private GameModel gameId;
+
+  @Column(nullable = false)
+  private LocalDate rentDate;
+
+  @Column(nullable = false)
+  private Long daysRented;
+
+  @Column(nullable = false)
+  private Optional<LocalDate> returnDate;
+}
